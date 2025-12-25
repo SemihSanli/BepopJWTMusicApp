50e7da2a15ebe2e1a00e6a6de6f08f9422013b59
Semih Şanlı <semih-semih-35@hotmail.com>
2025-12-25 15:13:03 +0300

    Order entity güncellemesi
    Order entitysine PaidAmount ve CreatedDate alanları eklendi ve migration atıldı

---
+++ BepopJWT.DataAccessLayer/Migrations/20251225121124_mig_order_entity_update.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BepopJWT.DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_order_entity_update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "PaidAmount",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PaidAmount",
+                table: "Orders");
+        }
+    }
+}
