218fd84270378fe55bfba1f68f172acf9ea2103f
Semih Şanlı <semih-semih-35@hotmail.com>
2025-12-28 20:56:42 +0300

    Playlist işlemleri
    Playlist işlemleri tamamlandı

---
+++ BepopJWT.API/Controllers/PlaylistsController.cs
@@ -0,0 +1,52 @@
+﻿using BepopJWT.BusinessLayer.Abstract;
+using BepopJWT.DTOLayer.PlaylistDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BepopJWT.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistsController : ControllerBase
+    {
+        private readonly IPlayListService _playlistService;
+
+        public PlaylistsController(IPlayListService playlistService)
+        {
+            _playlistService = playlistService;
+        }
+
+        // 1. KULLANICININ LİSTELERİNİ GETİR (Resimli, Şarkılı, İsimli)
+        [HttpGet("user/{userId}")]
+        public async Task<IActionResult> GetUserPlaylists(int userId)
+        {
+            // Manager'daki o özel DTO dönüşümünü çağırıyoruz
+            var values = await _playlistService.GetPlaylistsByUserIdAsync(userId);
+            return Ok(values);
+        }
+
+        // 2. YENİ LİSTE OLUŞTUR
+        [HttpPost("create")]
+        public async Task<IActionResult> CreatePlaylist([FromBody] CreatePlaylistDTO createPlaylistDto)
+        {
+            await _playlistService.CreatePlayListAsync(createPlaylistDto);
+            return StatusCode(201, "Playlist başarıyla oluşturuldu.");
+        }
+
+        // 3. LİSTEYE ŞARKI EKLE
+        [HttpPost("add-song")]
+        public async Task<IActionResult> AddSongToPlaylist([FromBody] AddSongToPlaylistDTO addSongToPlaylistDto)
+        {
+            await _playlistService.AddSongToPlaylistAsync(addSongToPlaylistDto);
+            return Ok("Şarkı listeye eklendi.");
+        }
+
+        // 4. (Opsiyonel) TÜM LİSTELERİ GÖR
+        [HttpGet]
+        public async Task<IActionResult> GetAllPlaylists()
+        {
+            var values = await _playlistService.TGetAllAsync();
+            return Ok(values);
+        }
+    }
+}
---
+++ BepopJWT.BusinessLayer/Concrete/SliderManager.cs
@@ -0,0 +1,46 @@
+﻿using BepopJWT.BusinessLayer.Abstract;
+using BepopJWT.DataAccessLayer.Abstract;
+using BepopJWT.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BepopJWT.BusinessLayer.Concrete
+{
+    public class SliderManager : ISliderService
+    {
+        private readonly ISliderDal _sliderDal;
+
+        public SliderManager(ISliderDal sliderDal)
+        {
+            _sliderDal = sliderDal;
+        }
+
+        public async Task TAddAsync(Slider entity)
+        {
+            await _sliderDal.AddAsync(entity);
+        }
+
+        public async Task TDeleteAsync(int id)
+        {
+            await _sliderDal.DeleteAsync(id);
+        }
+
+        public async Task<List<Slider>> TGetAllAsync()
+        {
+           return await _sliderDal.GetAllAsync();
+        }
+
+        public async Task<Slider> TGetByIdAsync(int id)
+        {
+            return await _sliderDal.GetByIdAsync(id);
+        }
+
+        public async Task TUpdateAsync(Slider entity)
+        {
+            await _sliderDal.UpdateAsync(entity);
+        }
+    }
+}
